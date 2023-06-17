8c418ad579f5433877f63b5444fd7f16f3e5774e
David Jordan <wordnerd92@gmail.com>
2023-06-17 11:09:10 -0500

    analytics and newsltetter sections are complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero"
+import Analytics from "./components/Analytics/Analytics";
+import Newsletter from "./components/Newsletter/Newsletter";
 
 function App() {
   return (
     <div className="App">
       <Navbar /> 
       <Hero />
+      <Analytics />
+      <Newsletter />
     </div>
   );
 }
